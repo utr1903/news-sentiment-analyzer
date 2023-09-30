67a52345b7a2042c5b51ca66eddd535b6451ddb3
uturkarslan <ugurturkarslan@gmail.com>
2023-09-30 19:40:54 +0200

    Implement OTLP exporting

--- app/otel/otel.py
+++ app/otel/otel.py
@@ -3,9 +3,8 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
-    SimpleSpanProcessor,
-    ConsoleSpanExporter,
 )
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 
 # Metrics
 from opentelemetry import metrics
@@ -14,23 +13,23 @@ from opentelemetry.sdk.metrics.export import (
     ConsoleMetricExporter,
     PeriodicExportingMetricReader,
 )
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+
 
 def init():
     # Traces
     provider = TracerProvider()
-    # processor = BatchSpanProcessor(ConsoleSpanExporter())
-    processor = SimpleSpanProcessor(ConsoleSpanExporter())
+    processor = BatchSpanProcessor(
+        OTLPSpanExporter()
+    )
     provider.add_span_processor(processor)
 
     # Sets the global default tracer provider
     trace.set_tracer_provider(provider)
 
     # Metrics
-    metric_reader = PeriodicExportingMetricReader(ConsoleMetricExporter())
+    metric_reader = PeriodicExportingMetricReader(OTLPMetricExporter())
     provider = MeterProvider(metric_readers=[metric_reader])
 
     # Sets the global default meter provider
     metrics.set_meter_provider(provider)
-
-    # Creates a meter from the global meter provider
-    meter = metrics.get_meter("my.meter.name")
